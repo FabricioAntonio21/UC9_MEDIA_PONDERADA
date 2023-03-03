4cb20be34a0f3afcd562985b33f65f4beb2437bd
FabricioAntonio21 <antoniofabricio016@gmail.com>
2023-03-02 21:12:57 -0300

    Att 2

--- UC9_MEDIA_PONDERADA/Program.cs
+++ UC9_MEDIA_PONDERADA/Program.cs
@@ -25,26 +25,26 @@ for (l = 0; l <= 4; l++)
     media[l, c++] = Console.ReadLine();
     c = 0;
 
-    total = (int.Parse(media[l, 3]) + int.Parse(media[l, 4]) + int.Parse(media[l, 5]) / 3) + trabalho;
-    presenca = (int.Parse(media[l, 2]) / int.Parse(media[l, 1])) * 100;
+    total = ((int.Parse(media[l, 3]) + int.Parse(media[l, 4]) + int.Parse(media[l, 5])) / 3 ) + trabalho;
+    presenca = (double.Parse(media[l, 2]) / double.Parse(media[l, 1])) * 100;
 
-    if (total >= 6 && presenca > 25)
+    if (total >= 6 && presenca <= 25)
     {
         resultado = "Aprovado";
     }
-    if (total < 6 && presenca >= 25)
+    if (total < 6 && presenca <= 25)
     {
         resultado = "Reprovado por Menção";
     }
-    if (total > 6 && presenca < 25)
+    if (total >= 6 && presenca > 25)
     {
         resultado = "Reprovado por Faltas";
     }
-    if (total < 6 && presenca < 25)
+    if (total <= 6 && presenca > 25)
     {
         resultado = "Reprovado por Menção e Faltas";
     }
 
-    Console.WriteLine(media[l, 0] + ", sua nota foi de: " + total + " sua presença foi de: " + presenca + ", o Aluno foi ");
+    Console.WriteLine(media[l, 0] + ", sua nota foi de: " + total + " sua cota de falta foi de: " + presenca + "%, o Aluno foi " + resultado);
 
 }
